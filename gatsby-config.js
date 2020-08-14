6765d8fddb019a73b0c2fa447b9ca300b0fb8398
okwasniewski <oskarkwasniewski@icloud.com>
2020-08-14 19:27:35 +0200

    Initial commit from gatsby: (https://github.com/thetrevorharmon/gatsby-starter-typescript-sass.git)

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    name: `Hello Typescript World!`,
+    tagline: `Gatsby + SASS + Typescript = 💪`
+  },  
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-tslint`,
+  ],
+}
