48b81a9e29527f89c2fd5552d05e1ed067bef984
okwasniewski <oskarkwasniewski@icloud.com>
2020-09-03 16:24:47 +0200

    feat: added yoast SEO integration

--- src/components/seo/index.tsx
+++ src/components/seo/index.tsx
@@ -6,10 +6,20 @@ type SEOTypes = {
   pageTitle: string
   description: string
   canonical?: string
+  ogImage?: any
+  keywords?: any
+  slug?: string
 }
-const Index = ({ pageTitle, description, canonical }: SEOTypes) => {
+const Index = ({
+  pageTitle,
+  description,
+  canonical,
+  ogImage,
+  keywords,
+  slug,
+}: SEOTypes) => {
   const { siteUrl, title } = useSiteMetadata()
-  const href = `${siteUrl}${pageTitle}`
+  const href = `${siteUrl}${slug}`
   return (
     <Hemlet>
       <html lang="pl" />
@@ -17,8 +27,9 @@ const Index = ({ pageTitle, description, canonical }: SEOTypes) => {
         name="viewport"
         content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
       />
-      <title>{`${title} - ${pageTitle}`}</title>
+      <title>{pageTitle}</title>
       <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
       {canonical ? (
         <link rel="canonical" href={`https://etechnologie.pl/${canonical}`} />
       ) : (
@@ -29,6 +40,9 @@ const Index = ({ pageTitle, description, canonical }: SEOTypes) => {
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="pl" />
       <meta property="og:site_name" content={title} />
+      <meta property="og:title" content={pageTitle} />
+      <meta property="og:image" content={siteUrl.concat(ogImage)} />
+      <meta property="og:description" content={description} />
     </Hemlet>
   )
 }
